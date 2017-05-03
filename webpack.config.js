0e26f37cdf35c2b92a3621e5a1abbe418b401a75
EGOmaniack <EGOmaniack@mail.ru>
2017-05-04 08:22:42 +1000

    2lsn

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,34 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: './dist/app.bundle.js'
+        filename: 'app.bundle.js'
+    },
+    module:{
+        rules:[{
+            test: /\.scss/,
+            use: ExtractTextPlugin.extract({
+                fallbackLoader: 'style-loader',
+                loader:['css-loader','sass-loader'],
+                publicPath: '/dist'
+            })
+        }]
     },
     plugins: [new HtmlWebpackPlugin({
         title: 'Custom template',
         minify: {
-            collapseWhitespace: true
+            collapseWhitespace: false
         },
         hash: true,
         template: './src/index.ejs', // Load a custom template (ejs by default see the FAQ for details)
+    }),
+    new ExtractTextPlugin({
+        filename: "styles.css",
+        disable: false,
+        allChunks: true
     })],
 };
