d253d05f2ff93889068f5c2a473aa61a7d622f77
EGOmaniack <EGOmaniack@mail.ru>
2017-05-03 23:13:10 +1000

    2 lsn git ignore generate html, path, html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
     entry: './src/app.js',
-    output:{
+    output: {
+        path: path.resolve(__dirname, "dist"),
         filename: './dist/app.bundle.js'
-    }
-}
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Custom template',
+        minify: {
+            collapseWhitespace: true
+        },
+        hash: true,
+        template: './src/index.ejs', // Load a custom template (ejs by default see the FAQ for details)
+    })],
+};
